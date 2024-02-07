362acf3093862757af219472bcf12e2d0e2366c7
Ahmed Saleh <ahmed1a1a3a100@gmail.com>
2024-02-07 03:01:59 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+"use strict";
+let score = 20;
+let highScore = 0;
+// create the random number
+let secret = Math.trunc(Math.random() * 20) + 1;
+const displayMsg = document.querySelector(".message");
+const displayScore = document.querySelector(".score");
+const displayHScore = document.querySelector(".highscore");
+const userNumber = document.querySelector(".number");
+let successAudio = new Audio("./succ.mp3");
+let winner = document.querySelector(".winner");
+// check the stored high score if it exit in local storage and assign it to highscore
+let storedHScore = Number(localStorage.getItem("highscore"));
+storedHScore ? (highScore = storedHScore) : highScore;
+displayHScore.textContent = highScore;
+
+// checking function
+function checking() {
+  const guess = Number(document.querySelector(".guess").value);
+  if (guess < 1 || guess > 20 || guess == '') {
+    displayMsg.textContent = "no number";
+  } else if (!(score <= 0)) {
+    // when user win
+    if (guess === secret) {
+      successAudio.play();
+      winner.classList.remove("hidden")
+      document.body.style.backgroundColor = "green";
+      userNumber.style.width = "30rem";
+      displayMsg.textContent = "مبروووك";
+      userNumber.textContent = secret;
+      score * 10 > highScore ? (highScore = score * 10) : highScore;
+      localStorage.setItem("highscore", String(highScore));
+      displayHScore.textContent = highScore;
+    }
+    // when user high
+    else if (guess > secret) {
+      score--;
+      displayScore.textContent = score;
+
+      displayMsg.textContent = "قربت بس اقل من كده شوية";
+    }
+    // when user low
+    else if (guess < secret) {
+      score--;
+      displayScore.textContent = score;
+      displayMsg.textContent = "قربت بس اعلى من كده شوية";
+    }
+  }
+  // lose game
+  else {
+    document.body.style.backgroundColor = "red";
+    userNumber.textContent = secret;
+    displayMsg.textContent = "للاسف خسرت .. اضغط على زرار اللعب مرة اخرى";
+    userNumber.style.width = "30rem";
+  }
+}
+// reset function
+function resetData() {
+  secret = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  displayScore.textContent = score;
+  userNumber.textContent = "?";
+  document.body.style.backgroundColor = "#222";
+  document.querySelector(".guess").value = "";
+  displayMsg.textContent = "ابدا تخمين";
+  userNumber.style.width = "15rem";
+  winner.classList.add("hidden")
+}
+// check btn
+document.querySelector(".check").addEventListener("click", function(){checking()});
+
+// again btn
+document.querySelector(".again").addEventListener("click",function(){resetData()});
