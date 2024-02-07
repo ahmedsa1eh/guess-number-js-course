b95048ae8a730c51ebeead084e8a094a11b158b0
Ahmed Saleh <ahmed1a1a3a100@gmail.com>
2024-02-07 21:05:14 +0200

    maintain and refacroring

--- app.js
+++ app.js
@@ -9,16 +9,20 @@ const displayHScore = document.querySelector(".highscore");
 const userNumber = document.querySelector(".number");
 let successAudio = new Audio("./succ.mp3");
 let winner = document.querySelector(".winner");
+//make messages changes when trying enter un valid number 
+let msgs = ["ادخل رقم بين 1 الى 20 فقط .. متتعبنيش معاك","ما قولنا ام 1 ل 20 يخى","حرام عليك اموت نفسى يمكن ترتاح قولت زفت 1 ل 20 بس"]
+let random = Math.ceil(Math.random() * msgs.length) - 1;
+
 // check the stored high score if it exit in local storage and assign it to highscore
 let storedHScore = Number(localStorage.getItem("highscore"));
 storedHScore ? (highScore = storedHScore) : highScore;
 displayHScore.textContent = highScore;
-
 // checking function
 function checking() {
   const guess = Number(document.querySelector(".guess").value);
   if (guess < 1 || guess > 20 || guess == '') {
-    displayMsg.textContent = "no number";
+    displayMsg.textContent = msgs[random];
+    random = Math.ceil(Math.random() * msgs.length) - 1 ;
   } else if (!(score <= 0)) {
     // when user win
     if (guess === secret) {
@@ -26,7 +30,7 @@ function checking() {
       winner.classList.remove("hidden")
       document.body.style.backgroundColor = "green";
       userNumber.style.width = "30rem";
-      displayMsg.textContent = "مبروووك";
+      displayMsg.textContent = "كسبت  مبروووك";
       userNumber.textContent = secret;
       score * 10 > highScore ? (highScore = score * 10) : highScore;
       localStorage.setItem("highscore", String(highScore));
@@ -64,7 +68,9 @@ function resetData() {
   document.querySelector(".guess").value = "";
   displayMsg.textContent = "ابدا تخمين";
   userNumber.style.width = "15rem";
-  winner.classList.add("hidden")
+  winner.classList.add("hidden");
+successAudio.pause();
+
 }
 // check btn
 document.querySelector(".check").addEventListener("click", function(){checking()});
